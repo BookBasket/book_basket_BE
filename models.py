ca3e3dede28445c213763bf989934ba3e126c237
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-04-04 13:46:09 -0600

    Add author migration
    Co-Authored-By: Rachel Lew <rlew07@gmail.com>
    Co-Authored-By: Madelyn Romero <m.romero013@gmail.com>

--- models.py
+++ models.py
@@ -13,3 +13,16 @@ class Book(db.Model):
 
 	def __repr__(self):
 		return '<id {}>'.format(self.id)
+
+
+class Author(db.Model):
+	__tablename__ = 'authors'
+
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String())
+
+	def __init__(self, name):
+		self.name = name
+
+	def __repr__(self):
+		return '<id {}>'.format(self.id)
