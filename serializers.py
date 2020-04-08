b3994bf15d99bf19f3ed818a5d4078694ac8f7ae
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-04-07 19:50:43 -0600

    Update serializer
    Co-authored-by: Madelyn Romero <51763489+madelynrr@users.noreply.github.com>

--- serializers.py
+++ serializers.py
@@ -1,9 +1,29 @@
-from marshmallow import Schema, fields, pprint
+from marshmallow_jsonapi import Schema, fields
 
 class BookSerializer(Schema):
+    id = fields.String()
     title = fields.String()
-    # authors = fields.List()
+    authors = fields.Relationship(many = True)
     description = fields.String()
     published_date = fields.String()
     image_url = fields.String()
-    # genres = fields.List()
+    genres = fields.Relationship(many = True)
+
+    class Meta:
+        type_ = 'book'
+
+
+class AuthorSerializer(Schema):
+    id = fields.String()
+    name = fields.String()
+
+    class Meta:
+        type_ = 'author'
+
+
+class GenreSerializer(Schema):
+    id = fields.String()
+    type = fields.String()
+
+    class Meta:
+        type_ = 'genre'
