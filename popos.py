a09e2fcf411220d30d9a3d78f6eaf3f2fc01e526
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-04-07 19:51:05 -0600

    Update popos
    Co-authored-by: Madelyn Romero <51763489+madelynrr@users.noreply.github.com>

--- popos.py
+++ popos.py
@@ -1,12 +1,11 @@
-# import collections
-
-# defd = collections.defaultdict(lambda : [])
-
 class Book:
 	def __init__(self, object):
 		self.title = object['volumeInfo'].setdefault('title', '')
 		self.author = object['volumeInfo'].setdefault('authors', [])
 		self.description = object['volumeInfo'].setdefault('description', '')
 		self.published_date = object['volumeInfo'].setdefault('publishedDate', '')
-		self.image_url = object['volumeInfo'].setdefault('imageLinks', {})
+		if 'imageLinks' in object['volumeInfo']:
+			self.image_url = object['volumeInfo']['imageLinks'].setdefault('thumbnail', '')
+		else:
+			self.image_url = ''
 		self.genres = object['volumeInfo'].setdefault('categories', [])
