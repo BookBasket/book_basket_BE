1ee5a4ae6494496bdfc99730de5c49fb45131eb6
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-04-09 22:06:56 -0600

    Update to List

--- serializers.py
+++ serializers.py
@@ -3,11 +3,11 @@ from marshmallow_jsonapi import Schema, fields
 class BookSerializer(Schema):
     id = fields.String()
     title = fields.String()
-    authors = fields.Raw()
+    authors = fields.List(fields.String())
     description = fields.String()
     published_date = fields.String()
     image_url = fields.String()
-    genres = fields.Raw()
+    genres = fields.List(fields.String())
 
     class Meta:
         type_ = 'book'
