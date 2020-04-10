c502deb71c6dd259c983eac17abc3fca18d635d4
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-04-09 21:32:55 -0600

    Update to return genres and authors as array

--- popos.py
+++ popos.py
@@ -1,11 +1,11 @@
 class Book:
 	def __init__(self, object):
 		self.title = object['volumeInfo'].setdefault('title', '')
-		self.authors = '::'.join(object['volumeInfo'].setdefault('authors', []))
+		self.authors = object['volumeInfo'].setdefault('authors', [])
 		self.description = object['volumeInfo'].setdefault('description', '')
 		self.published_date = object['volumeInfo'].setdefault('publishedDate', '')
 		if 'imageLinks' in object['volumeInfo']:
 			self.image_url = object['volumeInfo']['imageLinks'].setdefault('thumbnail', '')
 		else:
 			self.image_url = ''
-		self.genres = '::'.join(object['volumeInfo'].setdefault('categories', []))
+		self.genres = object['volumeInfo'].setdefault('categories', [])
