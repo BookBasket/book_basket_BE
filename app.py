f87a4289bf6642494cd5f8232b916233a0d0617d
Madelyn Romero <m.romero013@gmail.com>
2020-04-13 12:58:37 -0600

    Build out pseudo code for post request
    Co-Authored-By: Rachel Lew <rlew421@users.noreply.github.com>

--- app.py
+++ app.py
@@ -59,6 +59,48 @@ def search():
         result = serializer.dump(book_collection)
         return build_actual_response(jsonify(result))
 
+@app.route('/create_book', methods=['OPTIONS', 'POST'])
+def create_book():
+    if request.method == 'OPTIONS':
+        return build_preflight_response()
+    elif request.method == 'POST':
+        req = request.get_json()
+
+        params = request.args
+        shelf_id = params.get(shelf_id)
+        shelf = Shelves.query.filter_by(id=shelf_id)
+
+        title = params.get(title)
+        summary = params.get(summary)
+        image_url = params.get(image_url)
+        isbn = params.get(isbn)
+        published_date = params.get(published_date)
+
+        book = BookModel(title=title, summary=summary, image_url=image_url, isbn=isbn, published_date=published_date)
+        shelf.books.append(book)
+
+        params.get(author)
+        params.get(genre)
+        # shelf = Shelves.query.filter_by(id=)
+
+
+        # args:
+        # shelf_id (find shelf with this)
+        # book information (create book with this, associated with above shelf)
+        # author information (check/create author, associate with book)
+        # genre information (check/create genre, associate with book)
+
+
+
+
+
+        search = SearchFacade.from_query(request.args)
+        book_collection = search.books
+        serializer = BookSerializer(many = True)
+        result = serializer.dump(book_collection)
+        return build_actual_response(jsonify(result))
+
+
 
 def build_preflight_response():
     response = make_response()
