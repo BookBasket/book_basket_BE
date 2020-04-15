6ff02ca626d9060f04070d1943a250f5eafde06c
Rachel Lew <rlew07@gmail.com>
2020-04-15 15:31:22 -0600

    Get book images from OpenLibrary Covers API and reassign a book's image_url
    Co-Authored-By: Madelyn Romero <51763489+madelynrr@users.noreply.github.com>

--- facades.py
+++ facades.py
@@ -7,11 +7,15 @@ class SearchFacade():
 		self.query = query
 		self.books = books
 
+
 	@classmethod
 	def from_query(cls, query):
 		service = GoogleBooksService(query)
 		books = []
 		for book in service.get_json()['items']:
-			books.append(Book.from_google_response(book))
+			new_book = Book.from_google_response(book)
+			url = "http://covers.openlibrary.org/b/isbn/{}-M.jpg".format(new_book.isbn)
+			new_book.image_url = url
+			books.append(new_book)
 
 		return cls(query, books)
