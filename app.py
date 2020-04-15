105d0cba55010ca63e650100c9f67d6fcfb099f8
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-04-14 22:27:44 -0600

    Add flask cors

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import requests
 from flask_graphql import GraphQLView
 import json
 from flask_bcrypt import Bcrypt
-
+from flask_cors import CORS
 
 # dotenv
 from dotenv import load_dotenv
@@ -22,6 +22,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 # Modules
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+CORS(app)
 
 # Schema
 import schema
@@ -49,97 +50,88 @@ app.add_url_rule(
 
 @app.route('/search', methods=['OPTIONS', 'GET'])
 def search():
-    if request.method == 'OPTIONS':
-        return build_preflight_response()
-    elif request.method == 'GET':
-        req = request.get_json()
-        search = SearchFacade.from_query(request.args)
-        book_collection = search.books
-        serializer = BookSerializer(many = True)
-        result = serializer.dump(book_collection)
-        return build_actual_response(jsonify(result))
+    req = request.get_json()
+    search = SearchFacade.from_query(request.args)
+    book_collection = search.books
+    serializer = BookSerializer(many = True)
+    result = serializer.dump(book_collection)
+    return jsonify(result)
 
 @app.route('/create_book', methods=['OPTIONS', 'POST'])
 def create_book():
-    if request.method == 'OPTIONS':
-        return build_preflight_response()
-    elif request.method == 'POST':
+    params = request.args
+    isbn = params.get('isbn')
+    book = db.session.query(BookModel).filter_by(isbn=isbn).first()
+    if book:
+        serializer = BookSerializer()
+        result = serializer.dump(book)
+        return build_actual_response(jsonify(result))
+    else:
         params = request.args
+        shelf = ShelfModel.query.filter_by(id=2).first()
+
+        author_names = params.getlist('author')
+        authors = []
+        for author_name in author_names:
+            author_object = db.session.query(AuthorModel).filter_by(name=author_name).first()
+            if author_object:
+                authors.append(author_object)
+            else:
+                created_author = AuthorModel(name=author_name)
+                db.session.add(created_author)
+                authors.append(created_author)
+
+
+        genre_types = params.getlist('genre')
+        genres = []
+        for genre_type in genre_types:
+            genre_object = db.session.query(GenreModel).filter_by(type=genre_type).first()
+            if genre_object:
+                genres.append(genre_object)
+            else:
+                created_genre = GenreModel(type=genre_type)
+                db.session.add(created_genre)
+                genres.append(created_genre)
+
+        title = params.get('title')
+        summary = params.get('summary')
+        image_url = params.get('image_url')
         isbn = params.get('isbn')
-        book = db.session.query(BookModel).filter_by(isbn=isbn).first()
-        if book:
-            serializer = BookSerializer()
-            result = serializer.dump(book)
-            return build_actual_response(jsonify(result))
-        else:
-            params = request.args
-            shelf = ShelfModel.query.filter_by(id=2).first()
-
-            author_names = params.getlist('author')
-            authors = []
-            for author_name in author_names:
-                author_object = db.session.query(AuthorModel).filter_by(name=author_name).first()
-                if author_object:
-                    authors.append(author_object)
-                else:
-                    created_author = AuthorModel(name=author_name)
-                    db.session.add(created_author)
-                    authors.append(created_author)
-
-
-            genre_types = params.getlist('genre')
-            genres = []
-            for genre_type in genre_types:
-                genre_object = db.session.query(GenreModel).filter_by(type=genre_type).first()
-                if genre_object:
-                    genres.append(genre_object)
-                else:
-                    created_genre = GenreModel(type=genre_type)
-                    db.session.add(created_genre)
-                    genres.append(created_genre)
-
-            title = params.get('title')
-            summary = params.get('summary')
-            image_url = params.get('image_url')
-            isbn = params.get('isbn')
-            published_date = params.get('published_date')
-            book = BookModel(title=title, summary=summary, image_url=image_url, isbn=isbn, published_date=published_date, authors=authors, genres=genres, shelves=[shelf])
-            db.session.add(book)
-            db.session.commit()
-            serializer = BookSerializer()
-            result = serializer.dump(book)
-            return build_actual_response(jsonify(result))
-
-
-@app.route('/switch_shelves', methods=['OPTIONS', 'PATCH'])
-def switch_shelves():
-    if request.method == 'OPTIONS':
-        return build_preflight_response()
-    elif request.method == 'PATCH':
-        isbn = request.args.get('isbn')
-        book = db.session.query(BookModel).filter_by(isbn=isbn).first()
-        book.shelves.pop(0)
-        shelf = db.session.query(ShelfModel).filter_by(id=1).first()
-        book.shelves.append(shelf)
-
+        published_date = params.get('published_date')
+        book = BookModel(title=title, summary=summary, image_url=image_url, isbn=isbn, published_date=published_date, authors=authors, genres=genres, shelves=[shelf])
         db.session.add(book)
         db.session.commit()
         serializer = BookSerializer()
         result = serializer.dump(book)
-        return build_actual_response(jsonify(result))
+        return jsonify(result)
 
 
-def build_preflight_response():
-    response = make_response()
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add('Access-Control-Allow-Headers', "*")
-    response.headers.add('Access-Control-Allow-Methods', "*")
-    return response
+@app.route('/switch_shelves', methods=['OPTIONS', 'PATCH'])
+def switch_shelves():
+    isbn = request.args.get('isbn')
+    book = db.session.query(BookModel).filter_by(isbn=isbn).first()
+    book.shelves.pop(0)
+    shelf = db.session.query(ShelfModel).filter_by(id=1).first()
+    book.shelves.append(shelf)
+
+    db.session.add(book)
+    db.session.commit()
+    serializer = BookSerializer()
+    result = serializer.dump(book)
+    return jsonify(result)
 
 
-def build_actual_response(response):
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
+# def build_preflight_response():
+#     response = make_response()
+#     response.headers.add("Access-Control-Allow-Origin", "*")
+#     response.headers.add('Access-Control-Allow-Headers', "*")
+#     response.headers.add('Access-Control-Allow-Methods', "*")
+#     return response
+
+
+# def build_actual_response(response):
+#     response.headers.add("Access-Control-Allow-Origin", "*")
+#     return response
 
 if __name__ == '__main__':
     app.run()
