d8cd4063d2b2aa9440ce84c3f21984d26d858da5
Madelyn Romero <m.romero013@gmail.com>
2020-04-15 16:17:00 -0600

    Create initial test for create book response code
    Co-Authored-By: Rachel Lew <48839191+rlew421@users.noreply.github.com>

--- tests/app_test.py
+++ tests/app_test.py
@@ -18,3 +18,7 @@ class TestApp(unittest.TestCase):
 
 		json_data = json.loads(response.data)
 		self.assertEqual(40, len(json_data.get('data')))
+
+	def test_create_book_endpoint(self):
+		response = self.app.get('/create_book?title=cloud atlas&author=david mitchell&summary=An excellent book&isbn=9780739305058&image_url=http://covers.openlibrary.org/b/isbn/9780739305058-M.jpg&published_date=May 7 2001&genre=fiction')
+		self.assertEqual(200, response.status_code)
