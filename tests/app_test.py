aa41c7d7f0ec78c025919c1b22c55a2b4ebb0a46
Madelyn Romero <m.romero013@gmail.com>
2020-04-15 17:26:19 -0600

    Add models to testing to delete book before running create book test

--- tests/app_test.py
+++ tests/app_test.py
@@ -2,6 +2,7 @@ import unittest
 import requests
 import json
 from app import app
+from models import *
 
 class TestApp(unittest.TestCase):
 	def setUp(self):
@@ -40,8 +41,19 @@ class TestApp(unittest.TestCase):
 		json_data = json.loads(response.data)
 		self.assertEqual(40, len(json_data.get('data')))
 
-	def test_create_book_endpoint(self):
+	def test_create_book_endpoint_for_existing_book(self):
 		response = self.app.post('/create_book?title=Testing3&author=J K Rowling&summary=This is a test&image_url=not_a_real_image.jpeg&isbn=0&published_date=May 7 2001&genre=fiction&genre=fantasy')
+		self.assertEqual(200, response.status_code)
+
+		json_data = json.loads(response.data)
+		author_name = json_data['data']['attributes']['authors']['data'][0]['attributes']['name']
+		self.assertEqual(author_name, 'J K Rowling')
+
+	def test_create_book_endpoint_for_new_book(self):
+		book = BookModel.query.filter_by(isbn='12345').first()
+		db.session.delete(book)
+
+		response = self.app.post('/create_book?title=Testing3&author=J K Rowling&summary=This is a test&image_url=not_a_real_image.jpeg&isbn=12345&published_date=May 7 2001&genre=fiction&genre=fantasy')
 		self.assertEqual(200, response.status_code)
 
 		json_data = json.loads(response.data)
